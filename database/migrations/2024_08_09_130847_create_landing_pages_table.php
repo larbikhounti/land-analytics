b47a53f89c4ee77a08149ca4ea9a780eac830b1f
mohamed khounti <larbikhounti@gmail.com>
2024-08-09 14:20:32 +0100

    first init

---
+++ database/migrations/2024_08_09_130847_create_landing_pages_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('landing_pages', function (Blueprint $table) {
+            $table->id();
+            $table->string("url")->nullable();
+            $table->string("name");
+            $table->string("ad_id");
+            $table->foreignIdFor(User::class)->constrained()
+            ->cascadeOnDelete()->cascadeOnUpdate();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('landing_pages');
+    }
+};
