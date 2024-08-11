f21a944b75554569ee4280037eef6cd791a1bed6
mohamed khounti <larbikhounti@gmail.com>
2024-08-11 15:11:25 +0100

    updating SocialHandler class

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -4,22 +4,22 @@ namespace App\Http\Controllers\Auth;
 
 use App\Contracts\SocialServiceInterface;
 use App\Http\Controllers\Controller;
-use App\Services\SocialService;
 use Illuminate\Http\Request;
 
 
 class AuthController extends Controller
 {
 
-    function redirect(Request $request , SocialServiceInterface $socialService)
-    {
-       return $socialService->redirect($request->socialtype);
-       
-    }
+   function redirect(Request $request, SocialServiceInterface $socialService)
+   {
+      return $socialService->redirect($request->socialtype);
+   }
 
-    function callback(SocialServiceInterface $socialService)
-    {
-       return $socialService->callback();
+   function callback(SocialServiceInterface $socialService)
+   {
+      if($socialService->callback()){
+         return redirect()->route('/');
+      }
       
-    }
+   }
 }
