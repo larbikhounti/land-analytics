ff22f151acdef4876994131748e7a97fc4a4cc13
mohamed khounti <larbikhounti@gmail.com>
2024-08-11 15:12:56 +0100

    updating web

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
-use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -9,6 +8,10 @@ Route::get('/auth/redirect/{socialtype}',[AuthController::class,"redirect"]);
 
 Route::get('/', function () {
     return Auth::user();
+})->name("/");
+
+Route::get('/logout', function () {
+    return Auth::logout();
 });
- 
+
 Route::get('/auth/callback',[AuthController::class, "callback"]);
