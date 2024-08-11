e9c7ad0ff2fd1719b1f284e4feab93eac1ed551a
mohamed khounti <larbikhounti@gmail.com>
2024-08-11 13:32:03 +0100

    adding auth routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/auth/redirect/{socialtype}',[AuthController::class,"redirect"]);
+
 Route::get('/', function () {
-    return view('welcome');
+    return Auth::user();
 });
+ 
+Route::get('/auth/callback',[AuthController::class, "callback"]);
