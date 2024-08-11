ee4c8a4fda2782ef19be06cc6a97b5d54b4459de
mohamed khounti <larbikhounti@gmail.com>
2024-08-11 15:11:53 +0100

    updating and fixing SocialService

--- app/Services/SocialService.php
+++ app/Services/SocialService.php
@@ -5,41 +5,25 @@ namespace App\Services;
 use App\Contracts\SocialHandlerInterface;
 use App\Contracts\SocialServiceInterface;
 
-use App\Services\LoginOptions\google;
+class SocialService implements SocialServiceInterface
+{
 
-class SocialService implements SocialServiceInterface {
+    private $socialHandler;
 
-  
     public function __construct(SocialHandlerInterface $socialHandler)
     {
-       $this->socialHandler = $socialHandler;
+        $this->socialHandler = $socialHandler;
     }
 
-   public function redirect($socialtype) {
+    public function redirect($socialtype)
+    {
         session(['social_type' => $socialtype]);
-
-        switch ($socialtype) {
-            case 'google':
-                return  $this->socialHandler->HandleRedirect(new google);
-
-            default:
-                return redirect('/');
-        }
+        return  $this->socialHandler->HandleRedirect();
     }
 
-    public function callback() {
+    public function callback()
+    {
 
-        $socialtype = session('social_type');
-        
-        if (!$socialtype) {
-            return redirect('/');
-        }
-
-        switch ($socialtype) {
-            case 'google':
-               $this->socialHandler->Handlelogin(new google());
-            default:
-                return redirect('/');
-        }
+        return $this->socialHandler->Handlelogin();
     }
 }
