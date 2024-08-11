1a3e701b194551c134dd0da6044d8170e48752bd
mohamed khounti <larbikhounti@gmail.com>
2024-08-11 15:12:14 +0100

    updating google class

--- app/Services/LoginOptions/google.php
+++ app/Services/LoginOptions/google.php
@@ -3,6 +3,7 @@
 
 namespace App\Services\LoginOptions;
 
+use App\Contracts\GoogleRepositoryInterface;
 use App\Contracts\LoginOption;
 use App\Models\User;
 use Laravel\Socialite\Facades\Socialite;
@@ -10,9 +11,11 @@ use Illuminate\Support\Facades\Auth;
 
 class google implements LoginOption
 {
+    private $googleRepository;
 
-    function __construct()
+    public function  __construct(GoogleRepositoryInterface $googleRepository)
     {
+        $this->googleRepository = $googleRepository;
     }
     function redirect()
     {
@@ -21,22 +24,13 @@ class google implements LoginOption
 
     function Login()
     {
-        $googleUser = Socialite::driver('google')->user();
+        $gUser = Socialite::driver('google')->user();
 
-        $user = User::updateOrCreate([
-            'email' => $googleUser->email,
-        ], [
-            'google_id' => $googleUser->id,
-            'name' => $googleUser->name,
-            'google_token' => $googleUser->token,
-            'google_refresh_token' => $googleUser->refreshToken,
-            'ad_id' => $googleUser->id
-        ]);
-        
-        $isLogged = Auth::login($user, true);
-        if($isLogged){
-            return  redirect("/")->status(200);
-        }
+        $user = $this->googleRepository->saveUser($gUser);
+
+        Auth::login($user, true);
+
+        return  Auth::check();
         
     }
 }
