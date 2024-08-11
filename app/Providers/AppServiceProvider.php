34fd97b172852379bb176d58b7157e5bc8ff4696
mohamed khounti <larbikhounti@gmail.com>
2024-08-11 13:35:44 +0100

    binding contracts to their classes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Contracts\SocialHandlerInterface;
+use App\Contracts\SocialServiceInterface;
+use App\Services\SocialHandler;
+use App\Services\SocialService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +15,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(SocialHandlerInterface::class, SocialHandler::class);
+        $this->app->bind(SocialServiceInterface::class , SocialService::class);
     }
 
     /**
