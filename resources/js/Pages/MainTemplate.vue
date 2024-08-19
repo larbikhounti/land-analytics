2b777f7148f9060efe9f90dce628ba78ba200d00
mohamed khounti <larbikhounti@gmail.com>
2024-08-19 18:31:05 +0100

    playing around with slots

---
+++ resources/js/Pages/MainTemplate.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+
+<template>
+    <div id="main">
+
+        <slot />
+    </div>
+
+</template>
+
+<style>
+#main {
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    color: black;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+</style>
