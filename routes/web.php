358b069897b00b8f0a203d7c9e742477b053695f
mohamed khounti <larbikhounti@gmail.com>
2024-08-19 17:53:44 +0100

    referencing homeController

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,15 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
+
 
 Route::get('/auth/redirect/{socialtype}',[AuthController::class,"redirect"]);
 Route::get('/auth/callback',[AuthController::class, "callback"]);
 
-Route::get('/', function () {
-    return Inertia::render('Show', [
-        
-    ]);
-})->name("/");
+Route::get('/',[HomeController::class,"index"])->name("/");
 
 Route::get('/logout', function () {
     return Auth::logout();
