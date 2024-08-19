1f28b13cfcf07b33cd81fa444d9ebbbf5a375b6c
mohamed khounti <larbikhounti@gmail.com>
2024-08-19 15:22:13 +0100

    sorting things out

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,17 @@
 use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Illuminate\View\View;
 
 Route::get('/auth/redirect/{socialtype}',[AuthController::class,"redirect"]);
+Route::get('/auth/callback',[AuthController::class, "callback"]);
 
 Route::get('/', function () {
-    return Auth::user();
+    return View("welcome");
 })->name("/");
 
 Route::get('/logout', function () {
     return Auth::logout();
 });
 
-Route::get('/auth/callback',[AuthController::class, "callback"]);
+
