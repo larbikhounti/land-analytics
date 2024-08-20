51fcf42c77c5136e8de05fa81eff8e6643970098
mohamed khounti <larbikhounti@gmail.com>
2024-08-20 22:30:30 +0100

    adding Main component

---
+++ resources/js/Pages/Home/Main.vue
@@ -0,0 +1,19 @@
+<script setup>
+import MainTemplate from "./MainTemplate.vue";
+import Hero from "./Hero.vue";
+import Features from "./Features.vue";
+import About from "./About.vue";
+import Footer from "./Footer.vue";
+import Menu from "./components/Menu.vue";
+
+</script>
+
+<template>
+    <MainTemplate>
+     <Menu/>
+     <Hero/>
+     <Features/>
+     <About/>
+     <Footer/>
+    </MainTemplate> 
+</template>
