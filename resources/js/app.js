ac595c1208f8d2be003e547a49721f1ab667f753
mohamed khounti <larbikhounti@gmail.com>
2024-08-20 22:28:54 +0100

    renaming the project

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,7 +9,7 @@ createInertiaApp({
     const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
     return pages[`./Pages/${name}.vue`]
   },
-  title: title => title ? `${title} - Ping CRM` : 'Ping CRM',
+  title: title => title ? `${title} - Simo` : 'Simo',
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
