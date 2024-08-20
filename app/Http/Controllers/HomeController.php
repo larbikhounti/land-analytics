4377fdfdb5e46fbda605f72fc086faca7e1a0c3e
mohamed khounti <larbikhounti@gmail.com>
2024-08-20 22:28:13 +0100

    refactoring homeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,11 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Contracts\UserDtoInterface;
 use Inertia\Inertia;
+
 class HomeController extends Controller
 {
-    public function index(UserDtoInterface $user) {
-        return Inertia::render('Main', [
-            'user' => $user->getUser()
-        ]);
+    public function index(UserDtoInterface $user)
+    {
+        return Inertia::render('Home/Main');
     }
 }
