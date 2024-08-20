6b4a1fd1340c8401ba7a0bf22ca5391452a66fa8
mohamed khounti <larbikhounti@gmail.com>
2024-08-20 19:20:48 +0100

    adding tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+import colors from "tailwindcss/colors";
+module.exports = {
+  content: ["./resources/js/**/*.{vue,js}"],
+  darkMode: true, // or 'media' or 'class'
+
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
