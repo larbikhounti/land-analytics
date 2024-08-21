0e977281fd1cf294f199f2aa8d2e405c6f5649f1
mohamed khounti <larbikhounti@gmail.com>
2024-08-21 16:49:13 +0100

    component : login with google

---
+++ resources/js/Pages/Home/components/GoogleLogin.vue
@@ -0,0 +1,16 @@
+<script setup>
+import googleLogo from "@images/google-logo.svg";
+const props = defineProps(['authId'])
+</script>
+
+
+<template>
+    <a v-if="props.authId" href="#"
+        class="rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Dashboard
+        <span aria-hidden="true">&rarr;</span>
+    </a>
+    <a v-else href="/auth/redirect/google"
+        class="rounded-md  bg-white border border-black px-3.5 flex justify-center items-center align-centre py-2.5 text-sm font-semibold text-black shadow-sm hover:bg-red-500 focus-visible:outline hover:text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">
+        <img  class="w-8" :src="googleLogo" alt="" />Continue with Google
+    </a>
+</template>
