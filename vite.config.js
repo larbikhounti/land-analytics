ad77aeb7fd8378ca22f81a4f3ac645a087570e1f
mohamed khounti <larbikhounti@gmail.com>
2024-08-21 16:41:41 +0100

    adding aliases for importing images

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
@@ -8,8 +9,12 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        vue()
-    ],
+        vue()],
+    resolve: {
+            alias: {
+              '@images': path.resolve(__dirname, 'resources/images'), // Alias for the resources/images folder
+            },
+          },    
     // resolve: name => {
     //     const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
     //     return pages[`./Pages/${name}.vue`]
