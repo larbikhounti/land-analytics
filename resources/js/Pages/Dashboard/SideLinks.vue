e5d690024f03ad570d7d9eebe2ebf7e3a43a1d03
mohamed khounti <larbikhounti@gmail.com>
2024-08-22 15:32:24 +0100

    adding side links

---
+++ resources/js/Pages/Dashboard/SideLinks.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+
+import { ref } from 'vue'
+
+const links = ref([
+    {href : "#", content : "Home" },
+    {href : "#", content : "Pages" },
+    {href : "#", content : "Profile" }
+])
+</script>
+
+<template>
+   <!-- SideLinks -->
+          <nav class="mt-10 divide-y">
+            <Link v-for="link in links" :href="link.href" class="text-black  block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-200">
+              {{link.content}}
+            </Link>
+            <!-- Add more links here -->
+          </nav>
+       
+</template>
