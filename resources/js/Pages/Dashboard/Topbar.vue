b0e9409890330d54127f4a6ec59f146d66a76331
mohamed khounti <larbikhounti@gmail.com>
2024-08-22 15:33:03 +0100

    dash : showing auth user email

--- resources/js/Pages/Dashboard/Topbar.vue
+++ resources/js/Pages/Dashboard/Topbar.vue
@@ -1,11 +1,13 @@
-<script setup></script>
+<script setup>
+import { user } from "../countables/auth";
+</script>
 
 <template>
     <!-- Topbar -->
     <header class="bg-white shadow p-4">
         <div class="flex justify-between items-center">
             <h2 class="text-lg font-semibold text-gray-800">Dashboard</h2>
-            <button class="text-gray-800">Profile</button>
+            <button class="text-gray-400 font-bold">{{user.email}}</button>
         </div>
     </header>
 </template>
