c41b2edb15542473956bf709b43fb25ab2c10783
mohamed khounti <larbikhounti@gmail.com>
2024-08-22 15:02:53 +0100

    dashboard : adding routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\DashBoardController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -10,6 +11,7 @@ Route::get('/auth/redirect/{socialtype}',[AuthController::class,"redirect"]);
 Route::get('/auth/callback',[AuthController::class, "callback"]);
 
 Route::get('/',[HomeController::class,"index"])->name("/");
+Route::get('/dashboard',[DashBoardController::class,"index"])->name("/");
 
 Route::get('/logout', function () {
     return Auth::logout();
