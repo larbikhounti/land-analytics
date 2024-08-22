0ac1da3fff4ec15f1b3b85818b982168ac82e86e
mohamed khounti <larbikhounti@gmail.com>
2024-08-22 15:32:17 +0100

    dash : uptimizing sidebar

--- resources/js/Pages/Dashboard/Sidebar.vue
+++ resources/js/Pages/Dashboard/Sidebar.vue
@@ -1,19 +1,15 @@
-<script setup></script>
+<script setup>
+import Splitter from 'primevue/splitter';
+import SideLinks from './SideLinks.vue';
+
+</script>
 
 <template>
    <!-- Sidebar -->
    <aside class="w-64 bg-white shadow-lg">
         <div class="p-6">
-          <h1 class="text-2xl font-semibold text-gray-800">Dashboard</h1>
-          <nav class="mt-10">
-            <a href="#" class="text-black block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-200">
-              Home
-            </a>
-            <a href="#" class="text-black block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-200">
-              Analytics
-            </a>
-            <!-- Add more links here -->
-          </nav>
+          <h1 class="text-2xl font-semibold text-gray-800 ">Land analytics</h1>
+          <SideLinks/>
         </div>
       </aside>
 </template>
