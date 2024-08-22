2c4353826ba264235fef4a665e4d1b0177998eb7
mohamed khounti <larbikhounti@gmail.com>
2024-08-22 17:14:57 +0100

    dash : refactoring and adding analytics

--- resources/js/Pages/Dashboard/Dashboard.vue
+++ resources/js/Pages/Dashboard/Dashboard.vue
@@ -1,19 +1,28 @@
-<script>
+<script setup>
 import DashboardLayout from './DashboardLayout.vue';
-
-export default {
-  components: {
-    DashboardLayout,
-  },
-}
+import BestPages from './components/BestPages.vue';
 </script>
 
 <template>
     <DashboardLayout>
-      <div class="text-gray-800">
-        <!-- Add your dashboard content here -->
-        <h1 class="text-3xl font-bold">Welcome to your Dashboard!</h1>
-      </div>
+            <div class="flex flex-row justify-center gap-3 mb-3">
+                <div class=" text-gray-400 bg-slate-200 border border-gray-400 p-5 text-center">
+                  <h1 class="text-black font-bold text-3xl">Total Visitors</h1> 
+                  <p class="text-green-500 font-bold text-2xl">300</p>
+                </div>
+                <div class=" text-gray-400 bg-slate-200 border border-gray-400 p-5 text-center">
+                  <h1 class="text-black font-bold text-3xl">Total Clicks</h1> 
+                  <p class="text-green-500 font-bold  text-2xl">300</p>
+                </div>
+                <div class=" text-gray-400 bg-slate-200 border border-gray-400 p-5 text-center">
+                  <h1 class="text-black font-bold text-2xl">Average Click Time</h1> 
+                  <p class="text-green-500 font-bold text-2xl">300</p>
+                </div>
+            </div>
+            
+                <BestPages title="Best Pages"/>
+            
+            
+            
     </DashboardLayout>
-  </template>
-  
+</template>
