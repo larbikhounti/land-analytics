2a7123a5ca2d4da2c2cded9bcfc0d94628c44a78
mohamed khounti <larbikhounti@gmail.com>
2024-08-26 12:56:45 +0100

    adding base functions

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -2,9 +2,52 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\LandingPage;
 use Illuminate\Http\Request;
 
 class LandingPageController extends Controller
 {
-    //
+
+    function index()
+    {
+        return LandingPage::all();
+    }
+
+    function store(Request $request)
+    {
+
+        $request->validate([
+            'url' => 'string|url',
+            'name' => 'string'
+        ]);
+
+        $land = new LandingPage();
+        if (!$land->save($request->all())) {
+            return false;
+        }
+
+        return true;
+    }
+    function show() {}
+
+    function update(Request $request, LandingPage $page)
+    {
+        $request->validate([
+            'url' => 'string|url',
+            'name' => 'string'
+        ]);
+
+        if (!$page->update($request->all())) {
+            return false;
+        }
+    }
+
+    function destroy(LandingPage $page)
+    {
+        if (!$page->delete()) {
+            return false;
+        }
+
+        return true;
+    }
 }
