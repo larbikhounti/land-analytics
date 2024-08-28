d761f64a1b3c14b7724580f33b086eab92d57ac9
mohamed khounti <larbikhounti@gmail.com>
2024-08-28 17:37:07 +0100

    implamenting layout system

--- resources/js/Pages/Dashboard/Dashboard.vue
+++ resources/js/Pages/Dashboard/Dashboard.vue
@@ -1,28 +1,25 @@
+
+
 <script setup>
-import DashboardLayout from './DashboardLayout.vue';
+import { reactive } from 'vue'
 import BestPages from './components/BestPages.vue';
+import Stats from './components/Stats.vue';
+
+
+let stats = reactive([
+  {title : "Total Visitors", count : 300},
+  {title : "Total Clicks", count : 20},
+  {title : "Average Click Time", count : 10},
+])
+</script>
+<script>
+import DashboardLayout from './DashboardLayout.vue';
+export default {layout : DashboardLayout}
 </script>
 
+
 <template>
-    <DashboardLayout>
-            <div class="flex flex-row justify-center gap-3 mb-3">
-                <div class=" text-gray-400 bg-slate-200 border border-gray-400 p-5 text-center">
-                  <h1 class="text-black font-bold text-3xl">Total Visitors</h1> 
-                  <p class="text-green-500 font-bold text-2xl">300</p>
-                </div>
-                <div class=" text-gray-400 bg-slate-200 border border-gray-400 p-5 text-center">
-                  <h1 class="text-black font-bold text-3xl">Total Clicks</h1> 
-                  <p class="text-green-500 font-bold  text-2xl">10</p>
-                </div>
-                <div class=" text-gray-400 bg-slate-200 border border-gray-400 p-5 text-center">
-                  <h1 class="text-black font-bold text-2xl">Average Click Time</h1> 
-                  <p class="text-green-500 font-bold text-2xl">20 min</p>
-                </div>
-            </div>
-            
-                <BestPages title="Best performing "/>
-            
-            
-            
-    </DashboardLayout>
+    <Stats :stats='stats' />
+    <BestPages title="Best performing " />
+
 </template>
