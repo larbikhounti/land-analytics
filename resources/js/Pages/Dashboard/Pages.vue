b1ecf1ca4c17399a318f6a63dad2dc5c0a9414dd
mohamed khounti <larbikhounti@gmail.com>
2024-08-28 17:37:26 +0100

    adding Pages components

---
+++ resources/js/Pages/Dashboard/Pages.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+<script>
+import DashboardLayout from './DashboardLayout.vue';
+export default {layout : DashboardLayout}
+</script>
+
+<template>
+    <h1 class="text-black">pages</h1>
+</template>
