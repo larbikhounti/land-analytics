8f4522b9292f7ad1953319fb9a83d74398879d0b
mohamed khounti <larbikhounti@gmail.com>
2024-08-28 19:17:29 +0100

    adding some basic css

--- resources/js/Pages/Dashboard/Pages.vue
+++ resources/js/Pages/Dashboard/Pages.vue
@@ -5,7 +5,6 @@ const form = useForm({
     url: null,
     name: null,
 })
-
 let props = defineProps(['pages']);
 </script>
 <script>
@@ -14,17 +13,20 @@ export default { layout: DashboardLayout }
 </script>
 
 <template>
-    <form class="flex flex-col gap-3 p-5 w-52"  @submit.prevent="form.post('/pages')">
+    <h1 class="font-bold text-lg ">Landing Pages Management</h1>
+    
+    <form class="flex flex-col gap-3 p-5 w-60"  @submit.prevent="form.post('/pages')">
         <label for="input">Url</label>
-        <input name="url" type="text" v-model="form.url">
+        <input class="p-3 hover:border hover:border-black" name="url" type="text" v-model="form.url">
         <div v-if="form.errors.url">{{ form.errors.url }}</div>
         <label for="input">Name</label>
-        <input  name="name" type="text" v-model="form.name">
+        <input class="p-3 hover:border hover:border-black" name="name" type="text" v-model="form.name">
         <div v-if="form.errors.name">{{ form.errors.name }}</div>
-        <button class="border border-black p-2 hover:bg-black hover:text-white" type="submit" :disabled="form.processing">Save</button>
+        <button  class="border border-black p-2 hover:bg-black hover:text-white  " type="submit" :disabled="form.processing">{{ form.processing? "saving... ": "save" }}</button>
+
     </form>
 
     <ul>
-        <li v-for="page in props.pages" :key="page.id">{{ page.name  }} : {{ page.url }}</li>
+        <li class="transition delay-150 duration-300 ease-in-out" v-for="page in props.pages" :key="page.id">{{ page.name  }} : {{ page.url }}</li>
     </ul>
 </template>
