790f016f76e7884ac9fb11d4bcd4bd2ca3921668
mohamed khounti <larbikhounti@gmail.com>
2024-08-28 20:07:12 +0100

    remove unnecessary title

--- resources/js/Pages/Dashboard/Topbar.vue
+++ resources/js/Pages/Dashboard/Topbar.vue
@@ -6,7 +6,6 @@ import { user } from "../countables/auth";
     <!-- Topbar -->
     <header class="bg-white shadow p-4">
         <div class="flex justify-between items-center">
-            <h2 class="text-lg font-semibold text-gray-800">Dashboard</h2>
             <button class="text-gray-400 font-bold">{{user.email}}</button>
         </div>
     </header>
