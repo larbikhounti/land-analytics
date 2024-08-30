c925220b2bbd0ae8168b7ad479f40041f0fddc3b
mohamed khounti <larbikhounti@gmail.com>
2024-08-30 13:01:17 +0100

    adding pagination component

---
+++ resources/js/Pages/Dashboard/components/Pagination.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+
+const props = defineProps(['links'])
+console.log(props.links)
+</script>
+<template>
+    <div>
+      <Link
+        v-for="link in links"
+        :href="link.url"
+        v-html="link.label"
+        class="px-1"
+        :class="{ 'text-gray-500': ! link.url, 'font-bold' : link.active }"
+      />
+    </div>
+  </template>
+  
