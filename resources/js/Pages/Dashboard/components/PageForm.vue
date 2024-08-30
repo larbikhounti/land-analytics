c605ab52aebbdb4e2462ca615173510b71aac1bd
mohamed khounti <larbikhounti@gmail.com>
2024-08-30 14:50:37 +0100

    refactoring pageForm component

--- resources/js/Pages/Dashboard/components/PageForm.vue
+++ resources/js/Pages/Dashboard/components/PageForm.vue
@@ -1,25 +1,25 @@
 <script setup>
 import { useForm } from '@inertiajs/vue3'
+import { Link } from '@inertiajs/vue3'
 const form = useForm({
     url: null,
     name: null,
     tracked_button:null
 })
-let probs = defineProps(['isformVisible'])
-const emit = defineEmits(['showForm'])
-
-function handleClick() {
-  emit('showForm')
-}
 
 function submitForm() {
     form.post('/pages')
 }
 </script>
+<script>
+import DashboardLayout from "../DashboardLayout.vue"
+export default { layout: DashboardLayout }
+</script>
 
 <template>
-     <button @click="handleClick" class="button bg-black text-white my-2 max-w-28 p-2">Add  {{probs.isformVisible? '-' : '+'}}</button>
-    <form v-if="probs.isformVisible" class="flex flex-col gap-3 p-5 w-2/4 bg-white" @submit.prevent="submitForm()">
+<Link class="button bg-black text-white my-2 max-w-fit p-2" href="/pages" method="get" as="button" type="button"> Back </Link>
+
+    <form  class="flex flex-col gap-3 p-5 w-2/4 bg-white" @submit.prevent="submitForm()">
         <label for="url">Url</label>
         <input id="url" placeholder="Enter url" class="p-3 border-2 bg-gray-100" name="url" type="text"
             v-model="form.url">
