a84266242a9a0b73aee26cff79c075a809ce0ab2
mohamed khounti <larbikhounti@gmail.com>
2024-08-30 13:34:59 +0100

    fixing null links

--- resources/js/Pages/Dashboard/components/Pagination.vue
+++ resources/js/Pages/Dashboard/components/Pagination.vue
@@ -8,7 +8,7 @@ console.log(props.links)
     <div>
       <Link
         v-for="link in links"
-        :href="link.url"
+        :href="link.url ?? '#'"
         v-html="link.label"
         class="px-1"
         :class="{ 'text-gray-500': ! link.url, 'font-bold' : link.active }"
