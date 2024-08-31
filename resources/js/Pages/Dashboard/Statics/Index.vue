ae9f99d11693106c94b519b74c9180e76a670377
mohamed khounti <larbikhounti@gmail.com>
2024-08-31 18:36:38 +0100

    refactorying dashboard directory

---
+++ resources/js/Pages/Dashboard/Statics/Index.vue
@@ -0,0 +1,25 @@
+
+
+<script setup>
+import { reactive } from 'vue'
+import BestPages from './Components/BestPages.vue';
+import Stats from './Components/Stats.vue';
+
+
+let stats = reactive([
+  {title : "Total Visitors", count : 300},
+  {title : "Total Clicks", count : 20},
+  {title : "Average Click Time", count : 10},
+])
+</script>
+<script>
+import DashboardLayout from '../Layout/DashboardLayout.vue';
+export default {layout : DashboardLayout}
+</script>
+
+
+<template>
+    <Stats :stats='stats' />
+    <BestPages title="Best performing " />
+
+</template>
